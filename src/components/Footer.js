94f5203679027e68ff70b24a504131133359c667
huseyinerdin <erdinh61@gmail.com>
2022-12-25 15:01:03 +0300

    Footer Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,6 @@ const Footer = () => {
   return (
     <footer className="info">
       <p>
-        Style Created by :{" "}
-        <a href="https://d12n.me/">
-          Dmitry Sharabin
-        </a>
-      </p>
-      <p>
         Part of <a href="http://todomvc.com">TodoMVC</a>
       </p>
     </footer>
